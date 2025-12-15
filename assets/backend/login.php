a7e3666450d93864d1cc8455c8d501f31f24e131
Aguinaldo Konzo <aguinaldokonzo7@gmail.com>
2025-12-15 09:59:33 +0100

    Agora vai

---
+++ assets/backend/login.php
@@ -0,0 +1,62 @@
+<?php
+//conexao com o banco de dados
+
+$host = "localhost";
+$db = "LexKnow_DB";
+$userName = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:dbname=$db;host=$host",$userName,$password);
+} catch (PDOException $th) {
+    http_response_code(500);
+    echo json_encode(["erro" => "Erro na conexão com o banco"]);
+    exit();
+} catch(Exception $ex) {
+    echo("Erro genérico: ". $ex->getMessage());
+}
+
+//sessao
+session_start();
+
+//Recebe Json do FrontEnd
+$datas = json_decode(file_get_contents("php://input"), true);
+$email = $datas['email'];
+$senha = $datas['senha'];
+
+$cmd = $pdo->prepare("SELECT * FROM admin_tb WHERE email = :e");
+$cmd->bindValue(":e", $email);;
+$cmd->execute();
+
+if($cmd->rowCount() != 0) {
+    $usuario = $cmd->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['logado'] =true;
+    if($senha === $usuario['senha']) {
+        //Login bem sucedido
+        echo json_encode(["success" => true]); 
+    } else {
+        //Senha incorreta
+        echo json_encode([
+            "success" => false, 
+            "message" => "Senha incorreta. Tente novamente."
+        ]);
+    }
+} else {
+    echo json_encode([
+            "success" => false, 
+            "message" => "Informações nao encontradas"
+        ]);
+}
+
+
+// $usuario = $cmd->fetch(PDO::FETCH_ASSOC);
+// if($usuario['email'] && password_verify($senha, $usuario['senha'])) {
+//     echo json_encode(["success" => true]);
+// } else {
+//     echo json_encode([
+//         "success" => false, 
+//         "message" => "Email ou senha incorretos"
+//     ]);
+// }
+
+?>
--- lexKnow-estagio/Lexknow-main/assets/script/header.js
+++
@@ -1,7 +0,0 @@
-const botaoMenu = document.querySelector(".navegacao-botao")
-const links = document.querySelector(".navegacao-links")
-
-botaoMenu.addEventListener("click", (evt) => {
-    links.classList.toggle("mostrar-link")
-    console.log("clico no botao menu")
-})
