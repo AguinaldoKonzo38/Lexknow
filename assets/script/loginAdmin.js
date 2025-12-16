3a79634c92a807230bb68ff669fe0a94bd52a83b
Aguinaldo <akakkonzo38@gmail.com>
2025-12-16 08:41:25 +0100

    atualizações de tercça-feira

--- assets/script/loginAdmin.js
+++ assets/script/loginAdmin.js
@@ -1,22 +1,36 @@
-async function enviarDados(){
+async function enviarDados() {
     event.preventDefault()
     console.log("Clicou no botão entrar");
     const email = document.querySelector("#imail").value
     const senha = document.querySelector("#isenha").value
     console.log(`email: ${email} | senha: ${senha}`)
     try {
-        const enviar = await fetch("./caminho ainda não existe", {
+        const enviar = await fetch("http://localhost/LexKnow/Lexknow/assets/backend/login.php", {
             method: "POST",
-            body: JSON.stringify({email, senha})
+            body: JSON.stringify({ email, senha })
         });
 
-        //Resposta do servidor
+        // //Resposta do servidor
 
         const respostaServidor = await enviar.json()
-        if(respostaServidor.success)
+        if (respostaServidor.success == true)
             window.location.href = "dashboard.php"
-        else
-            alert("Verifique o seu email e senha!")
+        else {
+            Toastify({
+                text: respostaServidor.message,
+                duration: 3000,
+                // destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                     background: "#e74c3c",
+                },
+            }).showToast();
+
+        }
     } catch (Exception) {
         console.log("Aconteceu um erro genérico: ", Exception)
     }
